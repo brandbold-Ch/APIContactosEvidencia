ade64ef1a1f811ab8ed6b1af223606382b7d513a
Brandon Jared Molina Vazquez <223031@uptapachula.edu.mx>
2025-05-30 21:22:47 -0600

    added intern db url

--- config/orm.py
+++ config/orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session, sessionmaker, SessionTransaction
 from models.models import *
 
-engine = create_engine("postgresql://postgres:mypostgres@localhost:5432/postgres", echo=False)
+engine = create_engine("postgresql://contacts_cvlb_user:gMqXglwUcdhy5HPc7770cl1Vca9Yxj4y@dpg-d0t7b6be5dus73fj79l0-a/contacts_cvlb", echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, class_=Session)
 Base.metadata.create_all(engine)
 

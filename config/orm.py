d0498630dba308a48971366ef6dc7bbdafc029c3
Brandon Jared Molina Vazquez <223031@uptapachula.edu.mx>
2025-07-03 15:47:56 -0600

    Changed url db

--- config/orm.py
+++ config/orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session, sessionmaker, SessionTransaction
 from models.models import *
 
-engine = create_engine("postgresql://contacts_cvlb_user:gMqXglwUcdhy5HPc7770cl1Vca9Yxj4y@dpg-d0t7b6be5dus73fj79l0-a/contacts_cvlb", echo=False)
+engine = create_engine("postgresql://contacts_eg59_user:lVuak2JSKXe8Gl5EA281OrrpgtpaxFLq@dpg-d1jf8gidbo4c73c9f2o0-a/contacts_eg59", echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, class_=Session)
 Base.metadata.create_all(engine)
 
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.exception_handler(BaseHTTPException)
 def exception_handler(
         request: Request,
